918f236aa361668748cbb59af58a77d48770f2c8
SlashMe <pi3rra@root.gg>
2013-10-11 15:51:31 +0200

    Support dsa

--- key/key.py
+++ key/key.py
@@ -1,29 +1,45 @@
 #!/usr/bin/python
-import os, re
+import os, re, signal
+
+def cleanQuit(signum, frame):
+	quit()
+
+signal.signal(signal.SIGINT, cleanQuit)
 
 print "This isn't even the pre-pre-alpha phase, please don't attack this server."
 
 ok=False
 while ok == False :
-	print "Paste your one-line ssh publikey (RSA 2048 bits):"
-	entry = str(raw_input())
-	if entry[0:7] == "ssh-rsa":
-		entry = entry[8:]
-	key = entry[:372]
-	
-	if len(key) != 372:
-	        print "Bad key."
-		continue
-	
-	if re.match("^[A-Za-z0-9+/]*$", key):
-		line = "sudo /sschat/key/keycat.sh '"+key+"'"
-		os.system(line)
-		ok=True
-	else:
-	        print "Bad key."
-		continue
+        print "Paste your one-line ssh publikey (RSA 2048 bits or DSA):"
+	print "Something like ssh-rsa AAAAB3N... or ssh-dss AAAAB3N..."
+        entry = str(raw_input())
+        if entry[0:7] == "ssh-rsa":
+                entry = entry[8:]
+                key = entry[:372]
+                type = 1
+                if len(key) != 372:
+                        print "Bad key."
+                        continue
+        elif entry[0:7] == "ssh-dss":
+                entry = entry[8:]
+                key = entry[:580]
+                type = 2
+                if len(key) != 580:
+                        print "Bad key."
+                        continue
+        else:
+                print "Bad key."
+                continue
 
-print "\n\n Congratulations!"
+        if re.match("^[A-Za-z0-9+/]*$", key):
+                line = "sudo /sschat/key/keycat.sh '"+str(type)+"' '"+key+"'"
+                os.system(line)
+                ok=True
+        else:
+                print "Bad key."
+                continue
+
+print "\n\nCongratulations!"
 print "You can now connect to the user sschat."
 print "For now, you'll most likely find people in the channel : plop "
 print "\n\nPlease note that this server will not keep any trace of anything but your publikey."
--- key/keycat.sh
+++ key/keycat.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-line="ssh-rsa "$1
+if [[ $1 == '1' ]]
+then
+line="ssh-rsa "$2
+elif [[ $1 == '2' ]]
+then
+line="ssh-dss "$2
+else
+exit
+fi
 echo "$line" >> /sschat/.ssh/authorized_keys
