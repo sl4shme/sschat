05eb20663b31b2e64c5b0db63e0d2443f9e2bcb6
SlashMe <pi3rra@root.gg>
2013-10-22 10:23:49 +0200

    Modifications droits

---
+++ right.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+chown sschat:sschat ./*
+chown sschat:sschat .
+chown key:key key
+
+chmod 500 .
+chmod 500 key
+chmod 500 .ssh
+
+chmod 600 *.pyc
+chmod 600 bugReport
+chmod 400 *.py
+chmod 400 mot*
+chmod 400 README.md
+chmod u+x sschat.py
+
+chmod 500 key/key*
+chmod 400 key/text.py
+chmod 600 key/*.pyc
+chmod 400 key/.hushlogin
